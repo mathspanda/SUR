2803931051c2f488323231a3eb42af199cdb5e2b
mathspanda <mathspanda826@gmail.com>
2015-10-12 11:15:07 +0800

    add attach_policy

--- sur-coreos-cluster/create_bay_by_senlin_heat.py
+++ sur-coreos-cluster/create_bay_by_senlin_heat.py
@@ -4,6 +4,9 @@ from sur.client import SURClient
 from sur.action.senlin.clusters import Cluster
 from sur.action.senlin.nodes import Node
 from sur.action.senlin.profiles import Profile
+from sur.action.senlin.policies import ScalingInPolicy as SIPolicy
+from sur.action.senlin.policies import ScalingOutPolicy as SOPolicy
+from sur.action.senlin.webhooks import Webhook
 
 LOG = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -15,6 +18,24 @@ def wait_for_node_active(sc, node):
         if status == 'ACTIVE':
             break
         time.sleep(1)
+
+
+def attach_policy(sc, **params):
+    cluster_name = params.get('cluster_name', 'sur_cluster')
+    
+    # create scaling policy
+    si_policy_name = cluster_name + '_si_policy'
+    so_policy_name = cluster_name + '_so_policy'
+
+    si_policy_spec = params.get('si_policy_spec', None)
+    so_policy_spec = params.get('so_policy_spec', None)
+
+    SIPolicy.policy_create(sc, si_policy_name, si_policy_spec)
+    SOPolicy.policy_create(sc, so_policy_name, so_policy_spec)
+    time.sleep(1)
+
+    Cluster.cluster_policy_attach(sc, cluster_name, si_policy_name)
+    Cluster.cluster_policy_attach(sc, cluster_name, so_policy_name)
 
 
 def create_stack(sc, **params):
@@ -61,13 +82,26 @@ def create_stack(sc, **params):
             minion_profile_name)
         time.sleep(1)
 
+    # attach scaling policy
+    attach_policy(sc, **params)
+
+    # create scale-out webhook
+    wb = Webhook.cluster_webhook_create(sc, cluster_name + '_so_webhook',
+        cluster_name, 'CLUSTER_SCALE_OUT', {})
+    time.sleep(1)
+    wb_url = wb['webhook']['url']
+    LOG.info('webhook_url=%s' % wb_url)
+
+
 def main():
     sc = SURClient().setup_client()
     
     params = {
         'master_profile_spec': 'specs/heat_stack_random_string.yaml',
         'minion_profile_spec': 'specs/heat_stack_random_string.yaml',
-	'node_count': 2
+	'node_count': 2,
+	'si_policy_spec': 'specs/scaling_policy.spec',
+	'so_policy_spec': 'specs/scaling_policy.spec'
     }
     
     create_stack(sc, **params)	
